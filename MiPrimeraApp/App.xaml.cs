307b6b060fd629956b93a4c50a3092a28085ab62
Aldrix Diaz <aldrix.diaz@gmail.com>
2024-11-27 19:02:45 -0300

    Agregar archivos de proyecto.

---
+++ MiPrimeraApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MiPrimeraApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
